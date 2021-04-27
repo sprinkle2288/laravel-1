434047a3e28d4d84686e51e1f260dd3ab7864daa
cosintsev228@gmail.com <cositsev228@gmail.com>
2021-04-28 01:13:53 +0300

    Initial commit

---
+++ my-add-composerr/database/migrations/create_news_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->dateTime('publish_date');
+            $table->text('content');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
