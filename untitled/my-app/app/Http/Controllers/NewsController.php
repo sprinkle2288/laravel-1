ee3355fa2d8641a8559bc35e17b2dfcceacb7b82
cosintsev228@gmail.com <cositsev228@gmail.com>
2021-05-02 04:44:18 +0300

    тут задание "Забыл вчера дз прописать:              Реализовать CRUD для сущности, миграцию и модель которой вы делали в предыдущем ДЗ (news).              Необходимые методы:              index (список)              show (детальный)              store (создание)              update (обновление)              destroy (удаление)" И прошлое(исправил ошибки, в прошлом намудрил с названиями и сделал ошибки)

---
+++ untitled/my-app/app/Http/Controllers/NewsController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return News[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return News::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return News::create([
+            'title' => $request -> title,
+            'publish_date' => $request -> publish_date,
+            'contentt' => $request -> contentt,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function show(News $news)
+    {
+        return [$news->title, $news->publish_date, $news->contentt];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, News $news)
+    {
+        $news->title = $request->title;
+        $news->publish_date = $request->publish_date;
+        $news->contentt = $request->contentt;
+
+        $news->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(News $news)
+    {
+        $news->delete();
+    }
+}
